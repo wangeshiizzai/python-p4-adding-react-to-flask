37c6e82d90aa4e139c597d2e1a3cd00add65ba10
keshi <keshi.yvonne84@gmail.com>
2026-01-16 00:03:14 +0300

    completed

--- server/models.py
+++ server/models.py
@@ -7,8 +7,8 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String)
-    username = db.Column(db.String)
+    body = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
--- server/seed.py
+++ server/seed.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
 
 from random import choice as rc
-
 from faker import Faker
-
 from app import app
 from models import db, Message
 
 fake = Faker()
 
-usernames = [fake.first_name() for i in range(4)]
+# Create a small pool of usernames
+usernames = [fake.first_name() for _ in range(4)]
 if "Duane" not in usernames:
     usernames.append("Duane")
 
 def make_messages():
-
+    # Delete existing messages
     Message.query.delete()
     
     messages = []
-
-    for i in range(20):
+    for _ in range(20):
         message = Message(
             body=fake.sentence(),
             username=rc(usernames),
@@ -27,7 +25,8 @@ def make_messages():
         messages.append(message)
 
     db.session.add_all(messages)
-    db.session.commit()        
+    db.session.commit()
+    print("Seeded 20 messages with random usernames!")
 
 if __name__ == '__main__':
     with app.app_context():
